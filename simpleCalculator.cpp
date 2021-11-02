c46c8b5b1d60d9198126de061fd98029fa8d7774
Manish Mishra <manishk.mishra@tum.de>
2021-11-02 20:38:54 +0530

    adding add_subtract

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 
+double add(double a, double b)
+{
+	return a+b;
+}
+
+double subtract(double a, double b)
+{
+	return a-b;
+}
+
 int main()
 {
     std::cout<<"Hello Calculator"<<std::endl; 
